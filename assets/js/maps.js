5a1cd646f9ef93247e2df2c598bc0867e7a228c3
Cam-4587 Conroy <cameronconroy@live.com>
2024-08-19 13:25:57 +0000

    moved javascript code to maps.js

---
+++ assets/js/maps.js
@@ -0,0 +1,25 @@
+async function initMap() {
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766,
+        },
+        mapId: 'a8c9bd388635487f',
+    });
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+    var markers = locations.map(function (location, i) {
+        return new google.maps.marker.AdvancedMarkerElement({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    var markerClusterer = MarkerClusterer(map, markers,
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
